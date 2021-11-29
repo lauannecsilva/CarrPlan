90fc49dd9fc379ae3d651e66b23c2c8dfc31ca0b
Tiago Augusto <tiagoaugustoFront@gmail.com>
2021-11-29 00:16:32 -0300

    fix

---
+++ src/main/java/com/example/plataforma_estudo/dao/DisciplinaDao.java
@@ -0,0 +1,11 @@
+package com.example.plataforma_estudo.dao;
+
+import com.example.plataforma_estudo.models.Disciplina;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DisciplinaDao extends CrudRepository<Disciplina, String>{
+    
+}
